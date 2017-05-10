f5dc031fa31263d1136ca6b0edb53dce2ee3bc93
Shawn Erquhart <shawn@shawn.works>
2017-05-10 14:47:11 -0400

    split out EditorialWorkflowCardList component

---
+++ src/editorial-workflow/EditorialWorkflowCardList.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import moment from 'moment';
+import { partial } from 'lodash';
+import { DragSource } from 'react-simple-dnd';
+import { status } from '../constants/publishModes';
+import EditorialWorkflowCard from './EditorialWorkflowCard';
+import styles from './EditorialWorkflowCardList.css';
+
+const EditorialWorkflowCardList = ({ entries, requestDelete, requestPublish }) => {
+  return (
+    <div>
+      {
+        entries.map((entry) => {
+          // Look for an "author" field. Fallback to username on backend implementation;
+          const username = entry.getIn(['metaData', 'user']);
+          const author = entry.getIn(['data', 'author'], username);
+          const timeStamp = moment(entry.getIn(['metaData', 'timeStamp'])).format('llll');
+          const link = `collections/${ entry.getIn(['metaData', 'collection']) }/entries/${ entry.get('slug') }`;
+          const slug = entry.get('slug');
+          const ownStatus = entry.getIn(['metaData', 'status']);
+          const collection = entry.getIn(['metaData', 'collection']);
+          return (
+            <DragSource key={slug} slug={slug} collection={collection} ownStatus={ownStatus}>
+              <div className={styles.draggable}>
+                <EditorialWorkflowCard
+                  collection={collection}
+                  isModification={entry.get('isModification')}
+                  title={entry.getIn(['data', 'title'])}
+                  author={author}
+                  timeStamp={timeStamp}
+                  username={username}
+                  linkUrl={link}
+                  handleDeleteClick={partial(requestDelete, collection, slug, ownStatus)}
+                  allowPublish={ownStatus === status.last() && !entry.get('isPersisting', false)}
+                  handlePublishClick={partial(requestPublish, collection, slug, ownStatus)}
+                />
+              </div>
+            </DragSource>
+          );
+        })
+      }
+    </div>
+  );
+};
+
+export default EditorialWorkflowCardList;
--- src/editorial-workflow/EditorialWorkflowContent.js
+++ src/editorial-workflow/EditorialWorkflowContent.js
@@ -1,9 +1,8 @@
 import React, { PropTypes } from 'react';
-import { DragSource, DropTarget, HTML5DragDrop } from 'react-simple-dnd';
+import { DropTarget, HTML5DragDrop } from 'react-simple-dnd';
 import ImmutablePropTypes from 'react-immutable-proptypes';
-import moment from 'moment';
 import { status, statusDescriptions } from '../constants/publishModes';
-import EditorialWorkflowCard from './EditorialWorkflowCard';
+import EditorialWorkflowCardList from './EditorialWorkflowCardList';
 import styles from './EditorialWorkflowContent.css';
 
 class EditorialWorkflowContent extends React.Component {
@@ -56,43 +55,11 @@ class EditorialWorkflowContent extends React.Component {
       ));
     }
     return (
-      <div>
-        {
-          entries.map((entry) => {
-            // Look for an "author" field. Fallback to username on backend implementation;
-            const username = entry.getIn(['metaData', 'user']);
-            const author = entry.getIn(['data', 'author'], username);
-            const timeStamp = moment(entry.getIn(['metaData', 'timeStamp'])).format('llll');
-            const link = `collections/${ entry.getIn(['metaData', 'collection']) }/entries/${ entry.get('slug') }`;
-            const slug = entry.get('slug');
-            const ownStatus = entry.getIn(['metaData', 'status']);
-            const collection = entry.getIn(['metaData', 'collection']);
-            return (
-              <DragSource
-                key={slug}
-                slug={slug}
-                collection={collection}
-                ownStatus={ownStatus}
-              >
-                <div className={styles.draggable}>
-                  <EditorialWorkflowCard
-                    collection={collection}
-                    isModification={entry.get('isModification')}
-                    title={entry.getIn(['data', 'title'])}
-                    author={author}
-                    timeStamp={timeStamp}
-                    username={username}
-                    linkUrl={link}
-                    handleDeleteClick={this.requestDelete.bind(this, collection, slug, ownStatus)}
-                    allowPublish={ownStatus === status.last() && !entry.get('isPersisting', false)}
-                    handlePublishClick={this.requestPublish.bind(this, collection, slug, ownStatus)}
-                  />
-                </div>
-              </DragSource>
-            );
-          })
-        }
-      </div>
+      <EditorialWorkflowCardList
+        entries={entries}
+        requestDelete={this.requestDelete}
+        requestPublish={this.requestPublish}
+      />
     );
   };
 
