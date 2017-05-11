3a0de605c07e8a245de626a0d512382b758c3911
Shawn Erquhart <shawn@shawn.works>
2017-05-11 15:40:08 -0400

    rename editorial-workflow directory in camelcase

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from '../app/App';
 import HomeRedirect from './HomeRedirect';
-import EditorialWorkflowCheckpoint from '../editorial-workflow/EditorialWorkflowCheckpoint';
+import EditorialWorkflowCheckpoint from '../editorialWorkflow/EditorialWorkflowCheckpoint';
 import CollectionEntriesList from '../entriesList/CollectionEntriesList';
 import EntryPage from '../containers/EntryPage';
 import SearchEntriesList from '../entriesList/SearchEntriesList';
