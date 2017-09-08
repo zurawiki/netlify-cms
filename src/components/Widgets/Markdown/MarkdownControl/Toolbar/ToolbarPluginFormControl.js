e2e663778231c08ccf456564f2840140b955eae7
Shawn Erquhart <shawn@erquh.art>
2017-09-08 11:56:55 -0700

    add media library support for editor plugin controls

--- src/components/Widgets/Markdown/MarkdownControl/Toolbar/ToolbarPluginFormControl.js
+++ src/components/Widgets/Markdown/MarkdownControl/Toolbar/ToolbarPluginFormControl.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+import ImmutablePropTypes from "react-immutable-proptypes";
 import { resolveWidget } from '../../../../Widgets';
 import styles from './ToolbarPluginFormControl.css';
 
@@ -10,11 +11,13 @@ const ToolbarPluginFormControl = ({
   onRemoveAsset,
   getAsset,
   onChange,
+  mediaPaths,
+  onOpenMediaLibrary,
 }) => {
   const widget = resolveWidget(field.get('widget') || 'string');
   const key = `field-${ field.get('name') }`;
   const Control = widget.control;
-  const controlProps = { field, value, onAddAsset, onRemoveAsset, getAsset, onChange };
+  const controlProps = { field, value, onAddAsset, onRemoveAsset, getAsset, onChange, mediaPaths, onOpenMediaLibrary };
 
   return (
     <div className={styles.control} key={key}>
@@ -34,6 +37,8 @@ ToolbarPluginFormControl.propTypes = {
   onRemoveAsset: PropTypes.func.isRequired,
   getAsset: PropTypes.func.isRequired,
   onChange: PropTypes.func.isRequired,
+  mediaPaths: ImmutablePropTypes.map.isRequired,
+  onOpenMediaLibrary: PropTypes.func.isRequired,
 };
 
 export default ToolbarPluginFormControl;
