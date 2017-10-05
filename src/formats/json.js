22398830333c5f78818e8b07c85bbf11badb2ca9
Caleb <inchristalway-tech4him@yahoo.com>
2017-10-04 21:05:47 -0600

    Add support for sorting JSON keys.

--- src/formats/json.js
+++ src/formats/json.js
@@ -1,9 +1,13 @@
+import stableStringify from 'json-stable-stringify';
+
 export default class JSONFormatter {
   fromFile(content) {
     return JSON.parse(content);
   }
 
-  toFile(data) {
-    return JSON.stringify(data);
+  toFile(data, sortedKeys = []) {
+    const sortFn = sortKeys(sortedKeys);
+    const cmdFn = (a, b) => sortFn(a.key, b.key);
+    return stableStringify(data, { cmp: cmpFn });
   }
 }
